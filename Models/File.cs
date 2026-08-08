607cf74206824a36e251ecc7694aedd02c1eabc4
Pupler <misha.reak45@gmail.com>
2026-08-08 22:49:02 +0200

    feat(sorter): implement file model and update sorting logic to use it
    Grouping and file move logic still to be implemented.

---
+++ Models/File.cs
@@ -0,0 +1,7 @@
+namespace Sortify.Models;
+
+public class File
+{
+    public string? Name { get; set; }
+    public int DashIndex { get; set; }
+}
--- Services/FileSorterService.cs
+++ Services/FileSorterService.cs
@@ -5,18 +5,27 @@ public class FileSorterService
     public static void SortFiles(string path)
     {
         var files = Directory.GetFiles(path);
+        List<Models.File> files_list = [];
 
         foreach (var file in files)
         {
             string fileName = Path.GetFileNameWithoutExtension(file);
-            var indexLocated = fileName.IndexOf("-");
+            int dashIndex = fileName.IndexOf("-");
 
-            if (indexLocated == -1)
+            if (dashIndex == -1)
             {
                 Console.WriteLine($"{fileName}: Index \'-\' not found");
 
                 continue;
             }
+
+            var fileModel = new Models.File
+            {
+                Name = fileName,
+                DashIndex = dashIndex
+            };
+
+            files_list.Add(fileModel);
         }
     }
 }
